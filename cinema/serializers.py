f7c52ce6a7015a415d9b7d73c911743ced185e2a
vasylhladyshev404 <vasyl@nextgear.nl>
2025-12-25 11:53:10 +0100

    1

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -20,7 +20,7 @@ class MovieSerializer(serializers.Serializer):
     def create(self, validated_data):
         actors = validated_data.pop("actors", [])
         genres = validated_data.pop("genres", [])
-        movie = Movie.objects.create(**validated_data) 
+        movie = Movie.objects.create(**validated_data)
         movie.actors.set(actors)
         movie.genres.set(genres)
         return movie
--- cinema/urls.py
+++ cinema/urls.py
@@ -31,7 +31,11 @@ urlpatterns = [
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
     path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
-    path("cinema_halls/<int:pk>/", cinema_hall_detail, name="cinema-hall-detail"),
+    path(
+        "cinema_halls/<int:pk>/",
+        cinema_hall_detail,
+        name="cinema-hall-detail"
+    ),
 ]
 
 app_name = "cinema"
