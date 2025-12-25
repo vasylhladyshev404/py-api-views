979cc9622d7b016e084a7b0d08058c40300c2a31
vasylhladyshev404 <vasyl@nextgear.nl>
2025-12-25 10:57:56 +0100

    1

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -60,7 +60,7 @@ class GenreSerializer(serializers.Serializer):
 class CinemaHallSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=20)
-    row = serializers.IntegerField()
+    rows = serializers.IntegerField()
     seats_in_row = serializers.IntegerField()
 
     def create(self, validated_data):
@@ -68,7 +68,7 @@ class CinemaHallSerializer(serializers.Serializer):
 
     def update(self, instance, validated_data):
         instance.name = validated_data.get("name", instance.name)
-        instance.row = validated_data.get("row", instance.row)
+        instance.rows = validated_data.get("rows", instance.rows)
         instance.seats_in_row = validated_data.get(
             "seats_in_row",
             instance.seats_in_row
--- cinema/views.py
+++ cinema/views.py
@@ -50,7 +50,7 @@ def movie_detail(request, pk):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class GenreList(ApiView):
+class GenreList(APIView):
     def get(self, request):
         genres = Genre.objects.all()
         serializer = GenreSerializer(genres, many=True)
@@ -63,13 +63,24 @@ class GenreList(ApiView):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-class GenreDetail(ApiView):
+class GenreDetail(APIView):
     def get_object(self, pk):
         return get_object_or_404(Genre, pk=pk)
 
     def get(self, request, pk):
         genre = self.get_object(pk)
         serializer = GenreSerializer(genre)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def patch(self, request, pk):
+        genre = self.get_object(pk)
+        serializer = GenreSerializer(
+            genre,
+            data=request.data,
+            partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, pk):
@@ -112,6 +123,9 @@ class ActorDetail(
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
 
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
     def put(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
 
@@ -131,13 +145,6 @@ class CinemaHallViewSet(
     serializer_class = CinemaHallSerializer
 
 
-class MovieViewSet(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    viewsets.GenericViewSet
-):
+class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
